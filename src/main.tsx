02c2f3e68300937a0c573db77725c65f4885003a
Jing Xu (RainX) <i@rainx.cc>
2025-05-05 15:10:52 +0800

    feat: update index.html and add new dependencies for toast notifications and hooks

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { ProgressIndicator } from './components/progress-indicator.component';
 import { FirstSection } from './sections/first-section.component';
 import { StorySectionHometown } from './sections/story-section-hometown.component';
 import { StorySectionBasic } from './sections/story-section-basic.component';
+import { Toaster } from 'react-hot-toast';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <FirstSection />
     <StorySectionHometown />
     <StorySectionBasic />
+    <Toaster />
   </React.StrictMode>,
 );
